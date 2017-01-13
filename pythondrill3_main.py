62a3a36447f1f748fe9fe96af265b26e4205affd
tomstock01 <tstock2@gmail.com>
2017-01-13 12:35:24 -0800

    added if __name__ == "__main__":

--- pythondrill3_main.py
+++ pythondrill3_main.py
@@ -7,13 +7,9 @@ import time
 import shutil
 
 
-fromFolder = 'C:\\Users\\thoma\\Desktop\\folderc'
-toFolder = 'C:\\Users\\thoma\\Desktop\\folderd'
-
-
 def filecopy(src, dst):
     now = time.time()  # gets current time
-    docs = os.listdir(fromFolder) # searches source folder for documents in it
+    docs = os.listdir(src) # searches source folder for documents in it
     for i in docs:
         y = src + '\\' + i # gets complete file path from the folder with the documents
         modtime = os.stat(y).st_mtime # reads the last modified date of each file
@@ -22,7 +18,11 @@ def filecopy(src, dst):
             shutil.copy(y, dst) # copies file that were modified within last 24 hours
             print y
 
-filecopy(fromFolder, toFolder)
 
+def main():
+    fromFolder = 'C:\\Users\\thoma\\Desktop\\folderc'
+    toFolder = 'C:\\Users\\thoma\\Desktop\\folderd'
+    filecopy(fromFolder, toFolder)
 
-
+if __name__ == "__main__":
+    main()
