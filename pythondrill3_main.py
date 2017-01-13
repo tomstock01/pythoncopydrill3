bdc7c6dea5ca4f906913d316fea55eac67b4f15d
tomstock01 <tstock2@gmail.com>
2017-01-13 10:46:48 -0800

    cleaned up for loop

--- pythondrill3_main.py
+++ pythondrill3_main.py
@@ -9,9 +9,9 @@ import shutil
 
 now = time.time()  # gets current time
 docs = os.listdir(r'C:\Users\thoma\Desktop\folderc') # searches source folder for documents in it
-for i in range(len(docs)):
-    x = docs[i]
-    y = 'C:\\Users\\thoma\\Desktop\\folderc\\' + x # gets complete file path from the folder with the documents
+
+for i in docs:
+    y = 'C:\\Users\\thoma\\Desktop\\folderc\\' + i # gets complete file path from the folder with the documents
     modtime = os.stat(y).st_mtime # reads the last modified date of each file
     past24 = (now - 86400)  # gets the time of 24 hours ago
     if modtime > past24:  # compares the last modified time vs 24 hours ago
